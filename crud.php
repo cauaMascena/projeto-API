1909fb2cc103cabbf9b7d74cc0fb84f1cc5e6554
cauaMascena <cauasinho656@gmail.com>
2021-11-05 11:06:39 -0300

    sla

--- crud.php
+++ crud.php
@@ -5,36 +5,81 @@
 include('connection.php');
 
 ##FUNÇÃO DE LEITURA DE DADOS (SEM CRITÉRIOS) ## 
-function read($conn) {
+function read($conn)
+{
 
     $sql = "SELECT * FROM tbl_pessoa";
 
-    if($resultado = mysqli_query($conn, $sql)) {
+    if ($resultado = mysqli_query($conn, $sql)) {
 
         $dados = mysqli_fetch_all($resultado);
 
-        echo json_encode(array("status"=>"success", "data"=>$dados));
-
+        echo json_encode(array("status" => "success", "data" => $dados));
     } else {
-        echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
-
+        echo json_encode(array("status" => "error", "data" => mysqli_error($conn)));
     }
+}
 
+##FUNÇÃO DE LEITURA DE DADOS (COM CRITÉRIOS) ## 
+function readID($cod_pessoa, $conn)
+{
+
+    $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+    if ($resultado = mysqli_query($conn, $sql)) {
+
+        $dados = mysqli_fetch_all($resultado);
+
+        echo json_encode(array("status" => "success", "data" => $dados));
+    } else {
+        echo json_encode(array("status" => "error", "data" => mysqli_error($conn)));
+    }
+}
+
+##FUNÇÃO DE ATUALIZAÇÃO##
+
+function update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn)
+{
+
+    $sql = "UPDATE tbl_pessoa SET
+    nome ='$nome',
+    sobrenome = '$sobrenome',
+    email = '$email',
+    celular = '$celular',
+    fotografia ='$fotografia'
+    WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("status" => "success", "data" => "Dados alterados com sucesso"));
+    } else {
+        echo json_encode(array("status" => "error", "data" => mysqli_error($conn)));
+    }
 }
 
 ##FUNÇÃO DE INSERÇÃO##
 
-function create($nome, $sobrenome, $email, $celular, $fotgrafia, $conn) {
-    $sql = "INSERT INTO tbl_pessoa (nome, sobrenome, email, celular, fotografia
-            VALUES ('$nome', '$sobrenome','$email','$celular', '$fotgrafia')";
+function create($nome, $sobrenome, $email, $celular, $fotgrafia, $conn)
+{
+
+    $sql = "INSERT INTO tbl_pessoa (nome, sobrenome, email, celular, fotografia) VALUES ('$nome', '$sobrenome','$email','$celular', '$fotgrafia')";
 
     if (mysqli_query($conn, $sql)) {
-        echo json_encode(array("status"=>"success", "data"=>"Dados inseridos com sucesso"));
+        echo json_encode(array("status" => "success", "data" => "Dados inseridos com sucesso"));
     } else {
-        echo json_encode(array("status"=>"error", "data"=>"Erro ao inserir os dados"));
-
+        echo json_encode(array("status" => "error", "data" => "Erro ao inserir os dados"));
     }
 }
 
+##FUNÇÃO DE EXCLUSÃO##
 
-?>
+function delete($cod_pessoa, $conn) {
+
+    $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("status" => "success", "data" => "Dados excluídos com sucesso"));
+    } else {
+        echo json_encode(array("status" => "error", "data" => mysqli_error($conn)));
+    }
+
+}
--- index.php
+++ index.php
@@ -11,9 +11,18 @@ include('crud.php');
 ##RECUPERA O TIPO DE AÇÃO DE REQUISIÇÃO##
 $acao = $_REQUEST["acao"];
 
+
 ##ROTA DO READ##
 if($acao == "read") {
     read($conn);
+}
+
+##ROTA DO READ##
+if($acao == "readID") {
+
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    readID($cod_pessoa, $conn);
+
 }
 
 ##ROTA DO CREATE##
@@ -29,9 +38,26 @@ if($acao == "create") {
     create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
 }
 
+##ROTA DO UPDATE##
 
+if($acao == "update") {
 
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    $nome = $_REQUEST["nome"];
+    $sobrenome = $_REQUEST["sobrenome"];
+    $email = $_REQUEST["email"];
+    $celular = $_REQUEST["celular"];
+    $fotografia = $_REQUEST["fotografia"];
 
+    update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn);
+}
 
+##ROTA DO DELETE##
+
+if($acao == "delete") {
+
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    delete($cod_pessoa, $conn);
+}
 
 ?>
